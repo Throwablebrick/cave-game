f0c5b67b09c5b1038ee449238d97fed5fd7a250d
Maximillian <179729177+Throwablebrick@users.noreply.github.com>
2025-07-15 22:27:56 -0700

    got frog jump to work

--- scripts/frog.gd
+++ scripts/frog.gd
@@ -1,20 +1,16 @@
-extends RigidBody2D
+extends CharacterBody2D
 
-@onready var rigid_body_2d: CharacterBody2D = $"."
+@onready var frog: CharacterBody2D = $"."
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
-@onready var area_2d: Area2D = $Area2D
-@onready var player_detector: CollisionShape2D = $Area2D/CollisionShape2D
-@onready var timer: Timer = $Timer
 
 const SPEED = 400
 const JUMP_SPEED = -600
+const GRAVITY = 1500
 
 var found: bool
 var destination: Vector2
-var velocity: Vector2
-var jump_ready: bool
+var jump_ready = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,18 +19,27 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+	else:
+		velocity.x = 0
 	if found:
 		if jump_ready:
 			jump_ready = false
-			if rigid_body_2d.x>destination.x:
+			if frog.position.x>destination.x:
 				velocity.x = -SPEED
+			else:
+				velocity.x = SPEED
 			velocity.y = JUMP_SPEED
-
+	move_and_slide()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	found=true
 	destination=body.position
+	print('guy entered at (%d, %d)' % [destination.x, destination.y])
 
 
 func _on_timer_timeout() -> void:
-	jump_ready=true
+	if is_on_floor():
+		jump_ready=true
+		print('jump ready (%d, %d)' % [destination.x, destination.y])
