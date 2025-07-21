2663a5f22a76614791900b45aa8f8055e6c26bbd
maximillian potter <179729177+Throwablebrick@users.noreply.github.com>
2025-07-20 20:22:48 -0700

    tori made levels

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 @onready var checkpoint: AnimatedSprite2D = $checkpoint
 
-@export var next_level_path: String
+@export var next_level_path: String = "res://scenes/"
 
+func _ready() -> void:
+	checkpoint.play("default")
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	global.next_level(next_level_path)
--- scripts/sub_viewport.gd
+++ scripts/sub_viewport.gd
@@ -9,6 +9,5 @@ func _switch_levels(new_level):
 	remove_child(level)
 	level.call_deferred("free")
 	
-	var next_level = load(new_level)
-	level = new_level.instance()
+	var next_level = load(new_level).instantiate()
 	add_child(next_level)
